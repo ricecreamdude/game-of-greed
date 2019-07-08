8d438448d78af921ac8586f976af73c9f34606fc
Joshua Ho <ho.joshua4@gmail.com>
2019-07-08 16:37:26 -0700

    Create an array of 6 random dice values

---
+++ game_of_greed.py
@@ -0,0 +1,28 @@
+import random
+
+
+# Application should simulate rolling between 1 and 6 dice
+diceHand = [None] * 6
+
+# for die in diceHand:
+  
+  # randomInt = 
+  # print(randomInt)
+  #Create a random dice for each 
+
+
+for i,dice in enumerate(diceHand):
+  diceHand[i] = random.randint(1,6)
+
+print(diceHand)
+
+# Application should allow user to set aside dice each roll
+
+# Application should user to enter score per roll
+
+# Application should allow “banking” current score or rolling again.
+
+# Application should keep track of total score
+
+# Application should keep track of current round
+
